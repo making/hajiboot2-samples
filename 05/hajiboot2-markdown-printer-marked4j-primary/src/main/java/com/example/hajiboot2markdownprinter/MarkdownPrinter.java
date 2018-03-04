5f06fb433e6d48279101943f21c4845d7585b64b
Toshiaki Maki <tmaki@pivotal.io>
2018-03-05 03:25:00 +0900

    Add

---
+++ 05/hajiboot2-markdown-printer-marked4j-primary/src/main/java/com/example/hajiboot2markdownprinter/MarkdownPrinter.java
@@ -0,0 +1,30 @@
+package com.example.hajiboot2markdownprinter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+@Component
+public class MarkdownPrinter {
+	private final MarkdownRenderer renderer;
+
+	public MarkdownPrinter(MarkdownRenderer renderer) {
+		this.renderer = renderer;
+	}
+
+	public void print(InputStream stream) {
+		System.out.print("Input markdown: ");
+		try {
+			String markdown = StreamUtils.copyToString(stream, StandardCharsets.UTF_8);
+			String html = this.renderer.render(markdown);
+			System.out.println(html);
+		}
+		catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+}

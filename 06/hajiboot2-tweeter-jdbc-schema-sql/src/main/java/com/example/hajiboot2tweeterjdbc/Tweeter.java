52a1ee8108e1a869a83448bbac8ac1b4c289c03c
Toshiaki Maki <tmaki@pivotal.io>
2018-04-23 03:09:51 +0900

    up

--- 06/hajiboot2-tweeter-jdbc-schema-sql/src/main/java/com/example/hajiboot2tweeterjdbc/Tweeter.java
+++ 06/hajiboot2-tweeter-jdbc-schema-sql/src/main/java/com/example/hajiboot2tweeterjdbc/Tweeter.java
@@ -5,41 +5,37 @@ import java.time.Instant;
 import java.util.UUID;
 
 public class Tweeter implements Serializable {
-    private final UUID uuid;
-    private final String text;
-    private final String username;
-    private final Instant created;
+	private final UUID uuid;
+	private final String text;
+	private final String username;
+	private final Instant createdAt;
 
-    public Tweeter(UUID uuid, String text, String username, Instant created) {
-        this.uuid = uuid;
-        this.text = text;
-        this.username = username;
-        this.created = created;
-    }
+	public Tweeter(UUID uuid, String text, String username, Instant createdAt) {
+		this.uuid = uuid;
+		this.text = text;
+		this.username = username;
+		this.createdAt = createdAt;
+	}
 
-    public UUID getUuid() {
-        return uuid;
-    }
+	public UUID getUuid() {
+		return uuid;
+	}
 
-    public String getText() {
-        return text;
-    }
+	public String getText() {
+		return text;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public Instant getCreated() {
-        return created;
-    }
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
 
-    @Override
-    public String toString() {
-        return "Tweeter{" +
-                "uuid=" + uuid +
-                ", text='" + text + '\'' +
-                ", username='" + username + '\'' +
-                ", created=" + created +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Tweeter{" + "uuid=" + uuid + ", text='" + text + '\'' + ", username='"
+				+ username + '\'' + ", createdAt=" + createdAt + '}';
+	}
 }

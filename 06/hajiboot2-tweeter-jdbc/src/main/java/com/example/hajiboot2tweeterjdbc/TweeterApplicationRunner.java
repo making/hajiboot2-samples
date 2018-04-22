5bd3ff9454d10990ab839316b40d74861e350f66
Toshiaki Maki <tmaki@pivotal.io>
2018-04-23 01:48:05 +0900

    Add

---
+++ 06/hajiboot2-tweeter-jdbc/src/main/java/com/example/hajiboot2tweeterjdbc/TweeterApplicationRunner.java
@@ -0,0 +1,44 @@
+package com.example.hajiboot2tweeterjdbc;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TweeterApplicationRunner implements ApplicationRunner {
+	private final JdbcTemplate jdbcTemplate;
+
+	public TweeterApplicationRunner(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		this.jdbcTemplate.execute("CREATE TABLE tweets (\n" + //
+				"  uuid       VARCHAR(36) PRIMARY KEY,\n" + //
+				"  text       VARCHAR(255),\n" + //
+				"  username   VARCHAR(128),\n" + //
+				"  created_at TIMESTAMP\n" + //
+				");");
+		this.jdbcTemplate.update(
+				"INSERT INTO tweets(uuid, text, username, created_at) VALUES(?,?,?,?)",
+				UUID.randomUUID(), "Demo1", "making", Instant.now());
+		this.jdbcTemplate.update(
+				"INSERT INTO tweets(uuid, text, username, created_at) VALUES(?,?,?,?)",
+				UUID.randomUUID(), "Demo2", "making", Instant.now());
+		Long count = this.jdbcTemplate.queryForObject("SELECT count(*) FROM tweets",
+				Long.class);
+		System.out.println("Number of tweets: " + count);
+		List<Tweeter> tweets = this.jdbcTemplate.query(
+				"SELECT uuid, text, username, created_at FROM tweets",
+				(rs, i) -> new Tweeter(UUID.fromString(rs.getString("uuid")),
+						rs.getString("text"), rs.getString("username"),
+						rs.getTimestamp("created_at").toInstant()));
+		tweets.forEach(tweeter -> System.out.println(tweeter));
+	}
+}

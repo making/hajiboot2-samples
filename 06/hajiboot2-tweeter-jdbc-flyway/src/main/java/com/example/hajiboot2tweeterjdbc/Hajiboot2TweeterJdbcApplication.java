92cc8afc7d1748fe39a3cce99381bff9872b8691
Toshiaki Maki <tmaki@pivotal.io>
2018-05-03 04:38:32 +0900

    Add

---
+++ 06/hajiboot2-tweeter-jdbc-flyway/src/main/java/com/example/hajiboot2tweeterjdbc/Hajiboot2TweeterJdbcApplication.java
@@ -0,0 +1,12 @@
+package com.example.hajiboot2tweeterjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hajiboot2TweeterJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hajiboot2TweeterJdbcApplication.class, args);
+	}
+}

da6d160bab395c90be17af5d83dc6c3e3b9aa9d7
Toshiaki Maki <tmaki@pivotal.io>
2018-05-08 02:17:04 +0900

    Add

---
+++ 06/hajiboot2-tweeter-jdbc-transactional/src/main/java/com/example/hajiboot2tweeterjdbc/Tweet.java
@@ -0,0 +1,59 @@
+package com.example.hajiboot2tweeterjdbc;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Tweet implements Serializable {
+	private final UUID uuid;
+	private final String text;
+	private final String username;
+	private final Instant createdAt;
+
+	public Tweet(UUID uuid, String text, String username, Instant createdAt) {
+		this.uuid = uuid;
+		this.text = text;
+		this.username = username;
+		this.createdAt = createdAt;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	@Override
+	public String toString() {
+		return "Tweet{" + "uuid=" + uuid + ", text='" + text + '\'' + ", username='"
+				+ username + '\'' + ", createdAt=" + createdAt + '}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Tweet tweet = (Tweet) o;
+		return Objects.equals(uuid, tweet.uuid) && Objects.equals(text, tweet.text)
+				&& Objects.equals(username, tweet.username)
+				&& Objects.equals(createdAt, tweet.createdAt);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(uuid, text, username, createdAt);
+	}
+}
---
+++ 06/hajiboot2-tweeter-jdbc-transactional/src/main/java/com/example/hajiboot2tweeterjdbc/TweetMapper.java
@@ -0,0 +1,36 @@
+package com.example.hajiboot2tweeterjdbc;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TweetMapper {
+	private final JdbcTemplate jdbcTemplate;
+
+	public TweetMapper(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public int insert(Tweet tweet) {
+		return this.jdbcTemplate.update(
+				"INSERT INTO tweets(uuid, text, username, created_at) VALUES(?,?,?,?)",
+				tweet.getUuid(), tweet.getText(), tweet.getUsername(),
+				tweet.getCreatedAt());
+	}
+
+	public long count() {
+		return this.jdbcTemplate.queryForObject("SELECT count(*) FROM tweets",
+				Long.class);
+	}
+
+	public List<Tweet> findAll() {
+		return this.jdbcTemplate.query(
+				"SELECT uuid, text, username, created_at FROM tweets",
+				(rs, i) -> new Tweet(UUID.fromString(rs.getString("uuid")),
+						rs.getString("text"), rs.getString("username"),
+						rs.getTimestamp("created_at").toInstant()));
+	}
+}

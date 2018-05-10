103e38fe158b544d6b4484ee1ddf51ab2c409da8
Toshiaki Maki <tmaki@pivotal.io>
2018-05-11 02:27:54 +0900

    Add

---
+++ 06/hajiboot2-tweeter-spring-data-jpa/src/main/java/com/example/hajiboot2tweeterjpa/Tweet.java
@@ -0,0 +1,82 @@
+package com.example.hajiboot2tweeterjpa;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Tweet implements Serializable {
+	@Id
+	private UUID uuid;
+	private String text;
+	private String username;
+	private Instant createdAt;
+
+	public Tweet() {
+	}
+
+	public Tweet(UUID uuid, String text, String username, Instant createdAt) {
+		this.uuid = uuid;
+		this.text = text;
+		this.username = username;
+		this.createdAt = createdAt;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Instant createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public String toString() {
+		return "Tweet{" + "uuid=" + uuid + ", text='" + text + '\'' + ", username='"
+				+ username + '\'' + ", createdAt=" + createdAt + '}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Tweet tweet = (Tweet) o;
+		return Objects.equals(uuid, tweet.uuid);
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(uuid);
+	}
+}

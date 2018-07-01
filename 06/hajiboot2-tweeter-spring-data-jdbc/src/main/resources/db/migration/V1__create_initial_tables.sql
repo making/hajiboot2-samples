38db3a943c30492c1921bffc5d1999d553bac140
Toshiaki Maki <tmaki@pivotal.io>
2018-07-01 19:36:27 +0900

    Remove version column

--- 06/hajiboot2-tweeter-spring-data-jdbc/src/main/resources/db/migration/V1__create_initial_tables.sql
+++ 06/hajiboot2-tweeter-spring-data-jdbc/src/main/resources/db/migration/V1__create_initial_tables.sql
@@ -2,6 +2,5 @@ CREATE TABLE tweets (
   uuid       VARCHAR(36) PRIMARY KEY,
   text       VARCHAR(255),
   username   VARCHAR(128),
-  created_at TIMESTAMP,
-  version    INTEGER
+  created_at TIMESTAMP
 );

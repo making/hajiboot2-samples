49f2757392adb35b3b3bc72828e104b13b2c2f73
Toshiaki Maki <tmaki@pivotal.io>
2019-07-17 03:25:49 +0900

    Bump to Spring Boot 2.2.0.M4

--- 05/hajiboot2-markdown-printer-rest-template/src/main/java/hajiboot/Main.java
+++ 05/hajiboot2-markdown-printer-rest-template/src/main/java/hajiboot/Main.java
@@ -1,4 +1,4 @@
-package com.example.hajiboot2markdownprinter;
+package hajiboot;
 
 import org.springframework.web.client.RestTemplate;
 
---
+++ 05/hajiboot2-markdown-printer-rest-template/src/test/java/hajiboot/MarkdownPrinterTest.java
@@ -0,0 +1,34 @@
+package hajiboot;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(OutputCaptureExtension.class)
+class MarkdownPrinterTest {
+
+	@Test
+	void print(CapturedOutput capture /* (1) */) {
+		MarkdownRenderer markdownRenderer = Mockito.mock(MarkdownRenderer.class); // (2)
+		given(markdownRenderer.render(anyString())).willReturn("<p>Markdown!</p>"); // (3)
+
+		MarkdownPrinter printer = new MarkdownPrinter(markdownRenderer);
+		InputStream stream = new ByteArrayInputStream(
+				"Markdown!".getBytes(StandardCharsets.UTF_8)); // (4)
+		printer.print(stream);
+
+		assertThat(capture.toString()).contains("Input markdown:"); // (5)
+		assertThat(capture.toString()).contains("<p>Markdown!</p>");
+	}
+}

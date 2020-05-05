bbf24b160779b1b53fd23dc672b3de1b627063d5
Toshiaki Maki <tmaki@pivotal.io>
2020-05-06 03:11:38 +0900

    Bump to Spring Boot 2.2.6.RELEASE

--- 05/hajiboot2-markdown-printer-marked4j-conflict/src/test/java/hajiboot/Hajiboot2MarkdownPrinterApplicationTests.java
+++ 05/hajiboot2-markdown-printer-marked4j-conflict/src/test/java/hajiboot/Hajiboot2MarkdownPrinterApplicationTests.java
@@ -21,9 +21,12 @@ class Hajiboot2MarkdownPrinterApplicationTests {
 	void contextLoads(CapturedOutput capture) {
         System.setIn(new ByteArrayInputStream(
                 "Hello **Spring Boot**".getBytes(StandardCharsets.UTF_8))); // (2)
-        SpringApplication.run(Hajiboot2MarkdownPrinterApplication.class); // (3)
+        try {
+            SpringApplication.run(Hajiboot2MarkdownPrinterApplication.class); // (3)
+        } catch (RuntimeException ignored) {
+        }
         assertThat(capture.toString())
-                .contains("<p>Hello <strong>Spring Boot</strong></p>"); // (4)
+                .contains("Consider marking one of the beans as @Primary, updating the consumer to accept multiple beans, or using @Qualifier to identify the bean that should be consumed"); // (4)
 	}
 
 }

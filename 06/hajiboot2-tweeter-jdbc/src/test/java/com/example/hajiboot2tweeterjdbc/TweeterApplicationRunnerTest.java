d85b28aa6cbeaa9da84398ccf6d5fb0a84c31b86
Toshiaki Maki <tmaki@pivotal.io>
2020-05-06 14:28:49 +0900

    Bump to Spring Boot 2.2.6.RELEASE

--- 06/hajiboot2-tweeter-jdbc/src/test/java/com/example/hajiboot2tweeterjdbc/TweeterApplicationRunnerTest.java
+++ 06/hajiboot2-tweeter-jdbc/src/test/java/com/example/hajiboot2tweeterjdbc/TweeterApplicationRunnerTest.java
@@ -1,26 +1,25 @@
 package com.example.hajiboot2tweeterjdbc;
 
-import org.junit.Rule;
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.rule.OutputCapture;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(OutputCaptureExtension.class)
 public class TweeterApplicationRunnerTest {
-	@Rule
-	public OutputCapture capture = new OutputCapture();
 
 	@Test
-	public void contextLoads() {
+	void contextLoads(CapturedOutput capture) {
 		SpringApplication.run(Hajiboot2TweeterJdbcApplication.class);
 		String output = capture.toString();
 		assertThat(output).contains("Number of tweets: 2");
 		assertThat(output).containsPattern(
-				"Tweet\\{uuid=[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}, text='Demo1', username='making', createdAt=[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}Z\\}");
+				"Tweet\\{uuid=[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}, text='Demo1', username='making', createdAt=[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]+Z}");
 		assertThat(output).containsPattern(
-				"Tweet\\{uuid=[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}, text='Demo2', username='making', createdAt=[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}Z\\}");
+				"Tweet\\{uuid=[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}, text='Demo2', username='making', createdAt=[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]+Z}");
 	}
 
 }

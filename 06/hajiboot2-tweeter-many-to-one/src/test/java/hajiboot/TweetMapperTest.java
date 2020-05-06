b6f7e762114c9c456015eb80a841fe6c541c65b2
Toshiaki Maki <tmaki@pivotal.io>
2020-05-06 21:59:56 +0900

    Base from hajiboot2-tweeter-jdbc-flyway

---
+++ 06/hajiboot2-tweeter-many-to-one/src/test/java/hajiboot/TweetMapperTest.java
@@ -0,0 +1,48 @@
+package hajiboot;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest // (1)
+@Transactional // (2)
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL) /* (3) */
+public class TweetMapperTest {
+	private final TweetMapper tweetMapper;
+
+	public TweetMapperTest(JdbcTemplate jdbcTemplate /* (3) */) {
+		this.tweetMapper = new TweetMapper(jdbcTemplate);
+	}
+
+	@Test
+	void insertAndCount() {
+		int updated = tweetMapper
+				.insert(new Tweet(UUID.randomUUID(), "test", "foo", Instant.now()));
+		assertThat(updated).isEqualTo(1);
+		long count = tweetMapper.count();
+		assertThat(count).isEqualTo(1);
+	}
+
+	@Test
+	void insertAndFindAll() {
+		Tweet tweet1 = new Tweet(UUID.randomUUID(), "test", "foo", Instant.now());
+		Tweet tweet2 = new Tweet(UUID.randomUUID(), "test", "foo", Instant.now());
+
+		int updated1 = tweetMapper.insert(tweet1);
+		assertThat(updated1).isEqualTo(1);
+		int updated2 = tweetMapper.insert(tweet2);
+		assertThat(updated2).isEqualTo(1);
+
+		List<Tweet> tweets = tweetMapper.findAll();
+		assertThat(tweets).containsExactly(tweet1, tweet2);
+	}
+}
---
+++ 06/hajiboot2-tweeter-many-to-one/src/test/java/hajiboot/TweeterApplicationRunnerTest.java
@@ -0,0 +1,45 @@
+package hajiboot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.DefaultApplicationArguments;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(OutputCaptureExtension.class)
+public class TweeterApplicationRunnerTest {
+
+	@Test
+	void contextLoads(CapturedOutput capture) throws Exception {
+		TweetMapper tweetMapper = mock(TweetMapper.class); // (1)
+
+		given(tweetMapper.count()).willReturn(2L); // (2)
+		given(tweetMapper.insert(any())).willReturn(1); // (3)
+		Instant now = Instant.now();
+		UUID uuid1 = UUID.randomUUID();
+		UUID uuid2 = UUID.randomUUID();
+		given(tweetMapper.findAll())
+				.willReturn(Arrays.asList(new Tweet(uuid1, "test1", "making", now),
+						new Tweet(uuid2, "test2", "making", now)));
+
+		TweeterApplicationRunner tweeterApplicationRunner = new TweeterApplicationRunner(
+				tweetMapper);
+		tweeterApplicationRunner.run(new DefaultApplicationArguments(new String[] {}));
+		String output = capture.toString();
+		assertThat(output).contains("Number of tweets: 2");
+		assertThat(output).contains("Tweet{uuid=" + uuid1
+				+ ", text='test1', username='making', createdAt=" + now + "}");
+		assertThat(output).contains("Tweet{uuid=" + uuid2
+				+ ", text='test2', username='making', createdAt=" + now + "}");
+	}
+
+}

3dd5cb67ec12a7dd3a5cd12d283901e0191c63e6
Toshiaki Maki <tmaki@pivotal.io>
2020-05-06 18:02:59 +0900

    Rename

--- 06/hajiboot2-tweeter-spring-data-jpa-flyway/src/main/java/hajiboot/Hajiboot2TweeterApplication.java
+++ 06/hajiboot2-tweeter-spring-data-jpa-flyway/src/main/java/hajiboot/Hajiboot2TweeterApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Hajiboot2TweeterJpaApplication {
+public class Hajiboot2TweeterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Hajiboot2TweeterJpaApplication.class, args);
+		SpringApplication.run(Hajiboot2TweeterApplication.class, args);
 	}
 }

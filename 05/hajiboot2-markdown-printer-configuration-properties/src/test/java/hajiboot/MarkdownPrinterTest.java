168a7f8c6c9bf262d3ab93986ddfbae37d43992a
Toshiaki Maki <tmaki@pivotal.io>
2020-05-06 11:46:57 +0900

    Bump to Spring Boot 2.2.6.RELEASE

--- 05/hajiboot2-markdown-printer-configuration-properties/src/test/java/hajiboot/MarkdownPrinterTest.java
+++ 05/hajiboot2-markdown-printer-configuration-properties/src/test/java/hajiboot/MarkdownPrinterTest.java
@@ -1,33 +1,34 @@
 package hajiboot;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.mockito.Mockito;
-import org.springframework.boot.test.rule.OutputCapture;
-
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
-public class MarkdownPrinterTest {
-	@Rule
-	public OutputCapture capture = new OutputCapture();
+@ExtendWith(OutputCaptureExtension.class)
+class MarkdownPrinterTest {
 
 	@Test
-	public void print() {
-		MarkdownRenderer markdownRenderer = Mockito.mock(MarkdownRenderer.class);
-		given(markdownRenderer.render(anyString())).willReturn("<p>Markdown!</p>");
+	void print(CapturedOutput capture /* (1) */) {
+		MarkdownRenderer markdownRenderer = Mockito.mock(MarkdownRenderer.class); // (2)
+		given(markdownRenderer.render(anyString())).willReturn("<p>Markdown!</p>"); // (3)
 
 		MarkdownPrinter printer = new MarkdownPrinter(markdownRenderer);
 		InputStream stream = new ByteArrayInputStream(
-				"Markdown!".getBytes(StandardCharsets.UTF_8));
+				"Markdown!".getBytes(StandardCharsets.UTF_8)); // (4)
 		printer.print(stream);
 
-		assertThat(capture.toString()).contains("Input markdown:");
+		assertThat(capture.toString()).contains("Input markdown:"); // (5)
 		assertThat(capture.toString()).contains("<p>Markdown!</p>");
 	}
 }
